201c459a138d87745ea548149b9c65c667f3b6c3
wme7 <manuel.ade@gmail.com>
2013-07-26 20:51:43 +0800

    update filese

--- myf90/ESBGK/ESBGK/initmodule.f90
+++ myf90/ESBGK/ESBGK/initmodule.f90
@@ -5,4 +5,49 @@ implicit none
     !
 contains
 
+    subroutine Initial_Condition(id,rho_l,u_l,p_l,rho_r,u_r,p_r)
+    implicit none
+    ! Choose the initial condition to work by it ID number
+    integer, intent(in) :: id
+    real, intent(out)   :: rho_l,u_l,p_l,rho_r,u_r,p_r
+    ! Table with cases
+    select case (id)
+        case (1)
+           print *, 'Shocktube problem of G.A. Sod, JCP 27:1, 1978 ';
+            p_l   = 1.00; p_r   = 0.100;
+            u_l   = 0.75; u_r   = 0.000;
+            rho_l = 1.00; rho_r = 0.125;
+        case (2)
+            print *, 'Lax test case: M. Arora and P.L. Roe: JCP 132:3-11, 1997 ';
+            p_l   = 3.528; p_r   = 0.571;
+            u_l   = 0.698; u_r   = 0.000;
+            rho_l = 0.445; rho_r = 0.500;
+        case (3)
+            print *, 'Mach = 3 test case: M. Arora and P.L. Roe: JCP 132:3-11, 1997 ';
+            p_l   = 10.333; p_r   = 1.00;
+            u_l   =  0.920; u_r   = 3.55;
+            rho_l =  3.857; rho_r = 1.00;
+        case (4)
+            print *, 'Shocktube problem with supersonic zone ';
+            p_l   = 1.0;  p_r   = 0.02;
+            u_l   = 0.0;  u_r   = 0.00;
+            rho_l = 1.0;  rho_r = 0.02;
+        case (5)
+            print *, 'Contact discontinuity ';
+            p_l   = 0.5; p_r   = 0.5;
+            u_l   = 0.0; u_r   = 0.0;
+            rho_l = 1.0; rho_r = 0.6;
+        case (6)
+            print *, 'Stationary shock ';
+            p_l   =  1.0; p_r   = 0.1;
+            u_l   = -2.0; u_l   = -2.0;
+            rho_l =  1.0; rho_r = 0.125;
+        case default
+            print *, 'IC not available';
+    end select
+    print *, ' ';
+    end subroutine Initial_Condition
+
+    subroutine Build_Elements()
+
 end module initmodule
--- myf90/ESBGK/ESBGK/main.f90
+++ myf90/ESBGK/ESBGK/main.f90
@@ -17,9 +17,15 @@ program main
     implicit none
 
     ! Governing Parameters
+    integer, parameter  :: mo = 2       ! ooa in space
+    integer, parameter  :: mt = 2       ! ooa in time
     integer, parameter  :: quad = 2     ! 1: GH, 2: NC
-    integer, parameter  :: ic = 1       ! IC. see: initmodule
+    integer, parameter  :: ic = 1       ! Initial Condition, see: initmodule
+    integer, parameter  :: kflux = 1    ! 1: Roe, 2: LF, 3: LLF
+    real, parameter     :: tend = 0.2   ! terminal time
     real, parameter     :: cfl = 0.5    ! CFL Number
+    real, parameter     :: tau = 0.001  ! Relaxation Time
+    real, parameter     :: MM = 1       ! TVB constant M
 
     ! Quadrature Parameters / Velocity space points
       ! Gauss Hermite Quadrature (GH)
@@ -33,8 +39,9 @@ program main
       real :: k                         ! quadrature constant
       real, allocatable  :: w(:),nv(:)  ! Size will depend on the choose quadrature
 
-    ! Grid Arrays
-    integer, parameter  :: nx=20, ny=20     ! XY-grid size
+    ! Grid and IC Arrays
+    integer, parameter  :: nx=20,ny=20  ! XY-grid size
+    real :: rl,ul,pl,rr,ur,pr           ! left and right IC values
 
     ! Probability Distritubion Function Arrays
     real :: f,feq,f_next  ! Matrix Arrays
@@ -42,8 +49,12 @@ program main
     integer :: idnumber,np
     !character(len = 100) :: output_file = 'myresults.plt'
     !character(len = 100) :: output_file2 = 'myresults.plt'
-    real :: T1, T2
+    real :: T1, T2, T3  ! Computation times
 
+    ! Calculate CPU time
+    call cpu_time(T1);
+
+    Print *, 'Build Nv points using Selected Aquadrature method'; print *, ' ';
     select case (quad)
         case (1)
             ! allocate w and nv
@@ -53,25 +64,32 @@ program main
             ! add factor e^(x^2) to weights
             w = w*exp(nv*nv)
             ! Print message
-            print *, 'Number of GH points to be used: ',ngh
+            print *, 'Number of GH points to be used: ',ngh; print *, ' ';
         case (2)
             ! allocate w and nv
             allocate( w(nnc_real) ); allocate( nv(nnc_real) );
             ! compute w and nv
             call newton_cotes(-2.0,2.0,nnc_real,nodes,nv,w,k)
             ! Print message
-            print *, 'Number of NC points to be used',nnc_real
+            print *, 'Number of NC points to be used',nnc_real; print *, ' ';
         case default
             print *, 'quadrature not available'
     end select
 
     ! Chek point (uncomment to diplay variables)
-    call disp('nv = ',nv); print *, ' ';
-    call disp('w = ',w); print *, ' ';
-    call disp('k = ',k); print *, ' ';
+    !call disp('nv = ',nv); print *, ' ';
+    !call disp('w = ',w); print *, ' ';
+    !call disp('k = ',k); print *, ' ';
 
-    ! Calculate CPU time
-    call cpu_time(T1); print *, 'time for calcualations', T1, 'seconds.';
+    print *, 'Build Grid and Load initial condition (IC)'; print *, ' ';
+    call Initial_Condition(ic,rl,ul,pl,rr,ur,pr)
+
+    ! Chek point (uncomment to diplay variables)
+    call disp(' (rho,u,p) at the left  = ',reshape((/rl,ul,pl/),(/1,3/))  )
+    call disp(' (rho,u,p) at the right = ',reshape((/rr,ur,pr/),(/1,3/))  )
+    print *, ' ';
+
+
 
     ! write to tecplot file
     !call tecplot_write_open(output_file,idnumber) ! open output file and identified No.
